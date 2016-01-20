62ed454c57a5218a2c62d90325f224964ec40bf5
Kevin Depuydt <kevin.depuydt@supinternet.fr>
2016-01-20 09:45:32 +0100

    Add Template and Tools component (the Template Object use Twig)

---
+++ app/components/template/Template.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kévin
+ * Date: 26/12/2015
+ * Time: 11:17
+ */
+
+namespace App\Components\Template;
+
+
+class Template
+{
+    private $twig;
+    private $twigLoader;
+    private $vars;
+
+    public function __construct($templateDir, $cacheDir = false) {
+
+        /** Twig init */
+        $this->twigLoader = new \Twig_Loader_Filesystem($templateDir);
+        $this->twig = new \Twig_Environment($this->twigLoader, array(
+            'cache' => $cacheDir,
+        ));
+
+        $this->vars = [
+            'site_title' => SITE_TITLE
+        ];
+
+        $this->addCssFile(CSS_DIR.'global.css');
+    }
+
+    /** RENDER */
+
+    public function render($template)
+    {
+        echo $this->twig->render($template, $this->vars);
+        return true;
+    }
+
+    /** ADD VARS */
+
+    public function addVar($key, $value) {
+        $this->vars[$key] = $value;
+    }
+
+    public function addArrayVars($array) {
+        $this->vars = array_merge($this->vars, $array);
+    }
+
+    /** NEED */
+
+    public function addCssFile($pathToCss, $placeFirst = false) {
+
+        if ($placeFirst && isset($this->vars['assets']['css']))
+            array_unshift($this->vars['assets']['css'], $pathToCss);
+        else
+            $this->vars['assets']['css'][] = $pathToCss;
+    }
+
+    public function addJsFile($pathToJs, $placeFirst = false) {
+        if ($placeFirst)
+            array_unshift($this->vars['assets']['js'], $pathToJs);
+        else
+            $this->vars['assets']['js'][] = $pathToJs;
+    }
+
+    public function addBootstrap()
+    {
+        $this->addCssFile(BOOTSTRAP_DIR_CSS.'bootstrap.min.css', true);
+        $this->addCssFile(BOOTSTRAP_DIR_CSS.'bootstrap-theme.min.css', true);
+        $this->addJsFile(BOOTSTRAP_DIR_JS.'bootstrap.min.js');
+    }
+
+    public function addFontAwesome()
+    {
+        $this->addCssFile(FONT_AWESOME_CSS_DIR.'font-awesome.min.css');
+    }
+
+    public function addSlick()
+    {
+        $this->addCssFile(BASE_SITE.'assets/slick/slick.css');
+        $this->addCssFile(BASE_SITE.'assets/slick/slick-theme.css');
+        $this->addJsFile(BASE_SITE.'assets/slick/slick.min.js');
+    }
+
+}
---
+++ app/components/tools/Tools.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kévin
+ * Date: 16/12/2015
+ * Time: 09:48
+ */
+
+namespace App\Components\Tools;
+
+class Tools
+{
+    /**
+     * fonction de redirection
+     */
+    public function go($url, $time)
+    {
+        die('<meta http-equiv="refresh" content="'.$time.'; url='.$url.'">');
+    }
+
+    /**
+     * fonction d'upload de fichier
+     */
+    public function upload($file, $uploadPath, $maxFileSize = 2000000, $newName = false) {
+
+        $extensionAccepted = ['.png','.jpg','.jpeg','.gif'];
+        $fileExtension = strtolower(substr($file['name'],strripos($file['name'],'.')));
+        $id = uniqid(rand());
+        $fileNewName = (!$newName)?$id.$fileExtension:$newName;
+
+        $uploadError = [
+            1 => 'La taille du fichier téléchargé est supérieure à la limite autorisée',
+            2 => 'La taille du fichier téléchargé est supérieure à la limite autorisée',
+            3 => 'Le fichier n\'a pas été correctement téléchargé',
+            4 => 'Aucun fichier n\'a été téléchargé',
+            6 => 'Le dossier temporaire pour le transfert du fichier est manquant',
+            7 => 'Echec de l\'écriture du fichier sur le disque',
+            8 => 'Une extension PHP a arrété l\'envoi de fichier'
+        ];
+
+        // si erreur d'upload
+        if ($file['error'] > 0)
+            return ['success' => false, 'message' => (isset($uploadPath[$file['error']])?$uploadError[$file['error']]:'Erreur inconnue')];
+
+        // si le type du fichier n'est pas bon
+        if (!in_array($fileExtension, $extensionAccepted))
+            return ['success' => false, 'message' => 'le fichier n\'est pas une image'];
+
+        // si fichier trop gros
+        if ($file['size'] > $maxFileSize)
+            return ['success' => false, 'message' => 'le fichier est trop lourd, 2Mo max'];
+
+        // si jamais le fichier existe déjà on change son nom
+        while (file_exists($uploadPath.'/'.$fileNewName)) {
+            $id = uniqid(rand());
+            $fileNewName = $id.$fileExtension;
+        }
+
+        $filePathAndName = $uploadPath.$fileNewName;
+
+        $compressPathAndName = $uploadPath.'cp-'.$fileNewName;
+
+        $resultat = move_uploaded_file($file['tmp_name'],$filePathAndName);
+
+        if ($resultat) {
+            // add compress function here
+            if ($this->compress_image($filePathAndName, $compressPathAndName, 90))
+                return ['success' => true, 'message' => 'le fichier à bien été transféré et compressé', 'filename' => $fileNewName];
+            else
+                return ['success' => true, 'message' => 'le fichier à bien été transféré, mais non compressé', 'filename' => $fileNewName];
+        } else
+            return ['success' => false, 'message' => 'Une erreur est survenue lors du transfert'];
+    }
+
+    /**
+     * fonction de compression des images
+     */
+    public function compress_image($source_url, $destination_url, $quality) {
+        $info = getimagesize($source_url);
+        $image = null;
+
+        if ($info['mime'] == 'image/jpeg') $image = imagecreatefromjpeg($source_url);
+        elseif ($info['mime'] == 'image/gif') $image = imagecreatefromgif($source_url);
+        elseif ($info['mime'] == 'image/png') $image = imagecreatefrompng($source_url);
+
+        //save file
+        imagejpeg($image, $destination_url, $quality);
+
+        //return destination file
+        return $destination_url;
+    }
+
+    public function redirect($url, $time) {
+        die('<meta http-equiv="refresh" content="'.$time.'; url='.$url.'">');
+    }
+
+    /**
+     * get POST or GET value
+     */
+    public function getValue($name)
+    {
+        if (!empty($_POST[$name] && empty($_GET[$name])))
+            return $_POST[$name];
+        else if (empty($_POST[$name] && !empty($_GET[$name])))
+            return $_GET[$name];
+        else if (!empty($_POST[$name] && !empty($_GET[$name])))
+            return ['get' => $_GET[$name], 'post' => $_GET[$name]];
+        else
+            return false;
+    }
+
+    /**
+     * get POST value $name
+     */
+    public function getPostValue($name)
+    {
+        return !empty($_POST[$name]) ? $_POST[$name] : false;
+    }
+
+    /**
+     * get GET value $name
+     */
+    public function getGetValue($name)
+    {
+        return !empty($_GET[$name]) ? $_GET[$name] : false;
+    }
+}
